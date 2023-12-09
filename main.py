5c50f0e41ae9c5bd6dfd7b318212ea9fddc234cf
Zufishan Waqar <76565226+Zufishan5@users.noreply.github.com>
2023-12-09 17:21:45 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-print(
-    "Twinkle Twinkle Little Star \nHow I Wonder what you are \nUp above the world so high \nLike a Diamond in the Sky"
-)
-# Assignment No 1
-print("I am Zufishan")
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers.
+# Your program should format the output in a readable manner!
+
+a = 15
+b = 20
+ans1 = a + b
+print("Addition of", a, "and", b, "is", ans1)
+print("Subtraction of", a, "and", b, "is:", a - b)
+print("Multiplication of", a, "and", b, "is:", a * b)
+print("Division of", a, "and", b, "is:", a / b)
+
+
+
+a = int(input("Enter a First Number:"))
+b = int(input("Enter a First Second:"))
+
+ans1 = a + b
+print("Addition of", a, "and", b, "is", ans1)
+print("Subtraction of", a, "and", b, "is:", a - b)
+print("Multiplication of", a, "and", b, "is:", a * b)
+print("Division of", a, "and", b, "is:", a / b)
